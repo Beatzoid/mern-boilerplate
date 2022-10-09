5c1becd3365e7380fe8df5981b5fa4cfd684c5a8
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-10-09 08:45:05 -0700

    :sparkles: User Model

---
+++ server/src/models/user.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            maxLength: 50
+        },
+        email: {
+            type: String,
+            trim: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            minLength: 5
+        },
+        lastName: {
+            type: String,
+            maxLength: 50
+        },
+        role: {
+            type: Number,
+            default: 0
+        },
+        token: {
+            type: String
+        },
+        tokenExp: {
+            type: Number
+        }
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("users", userSchema);
